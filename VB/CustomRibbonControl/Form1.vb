a6d1a207f1aa608d1215b1bf2caea7a398c84de6
DevExpressExampleBot <fakeEmail>
2023-09-13 17:34:49 +0400

    Source auto update [skip ci]

--- VB/CustomRibbonControl/Form1.vb
+++ VB/CustomRibbonControl/Form1.vb
@@ -1,23 +1,14 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
 Imports System.Windows.Forms
-Imports DevExpress.XtraBars.Ribbon
-Imports DevExpress.XtraBars
-Imports DevExpress.XtraBars.Ribbon.ViewInfo
 
 Namespace CustomRibbonControl
 
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
+    Public Partial Class Form1
+        Inherits Form
 
-		End Sub
-	End Class
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
 End Namespace
--- VB/CustomRibbonControl/RibbonControlDescendant.vb
+++ VB/CustomRibbonControl/RibbonControlDescendant.vb
@@ -1,59 +1,55 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports DevExpress.XtraBars.Ribbon
 Imports DevExpress.XtraBars.Ribbon.ViewInfo
-Imports System.Runtime.InteropServices
 Imports System.Drawing
 
 Namespace CustomRibbonControl
-	Public Class RibbonControlDescendant
-		Inherits RibbonControl
-		Private _BarItemExtender As BarItemExtender
-		Private viewInfo As RibbonViewInfoDescendant
 
-		Public Property BarItemExtender() As BarItemExtender
-			Get
-				Return _BarItemExtender
-			End Get
-			Set(ByVal value As BarItemExtender)
-				_BarItemExtender = value
-				If value IsNot Nothing Then
-					AddHandler _BarItemExtender.LocationChanged, AddressOf OnItemLocationChanged
-				End If
-				Me.Refresh()
-			End Set
-		End Property
-		Public Sub New()
-			MyBase.New()
-		End Sub
-		Private Sub OnItemLocationChanged(ByVal sender As Object, ByVal e As EventArgs)
-			Me.Refresh()
-		End Sub
-		Protected Overrides Function CreateViewInfo() As RibbonViewInfo
-			viewInfo = New RibbonViewInfoDescendant(Me)
-			Return CType(viewInfo, RibbonViewInfo)
-		End Function
-		Protected Overrides Overloads Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso _BarItemExtender IsNot Nothing Then
-				RemoveHandler _BarItemExtender.LocationChanged, AddressOf OnItemLocationChanged
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
-		Protected Overrides Sub OnGroupAdded(ByVal group As RibbonPageGroup)
-			MyBase.OnGroupAdded(group)
-			If group.AllowMinimize Then
-				group.AllowMinimize = False
-			End If
-		End Sub
+    Public Class RibbonControlDescendant
+        Inherits RibbonControl
 
-		Protected Overrides Sub OnGroupChanged(ByVal pageGroup As RibbonPageGroup)
-			MyBase.OnGroupChanged(pageGroup)
-			If pageGroup.AllowMinimize Then
-				pageGroup.AllowMinimize = False
-			End If
-		End Sub
-	End Class
+        Private _BarItemExtender As BarItemExtender
+
+        Private viewInfo As RibbonViewInfoDescendant
+
+        Public Property BarItemExtender As BarItemExtender
+            Get
+                Return _BarItemExtender
+            End Get
+
+            Set(ByVal value As BarItemExtender)
+                _BarItemExtender = value
+                If value IsNot Nothing Then AddHandler _BarItemExtender.LocationChanged, AddressOf OnItemLocationChanged
+                Refresh()
+            End Set
+        End Property
+
+        Public Sub New()
+            MyBase.New()
+        End Sub
+
+        Private Sub OnItemLocationChanged(ByVal sender As Object, ByVal e As EventArgs)
+            Refresh()
+        End Sub
+
+        Protected Overrides Function CreateViewInfo() As RibbonViewInfo
+            viewInfo = New RibbonViewInfoDescendant(Me)
+            Return CType(viewInfo, RibbonViewInfo)
+        End Function
+
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso _BarItemExtender IsNot Nothing Then RemoveHandler _BarItemExtender.LocationChanged, AddressOf OnItemLocationChanged
+            MyBase.Dispose(disposing)
+        End Sub
+
+        Protected Overrides Sub OnGroupAdded(ByVal group As RibbonPageGroup)
+            MyBase.OnGroupAdded(group)
+            If group.AllowMinimize Then group.AllowMinimize = False
+        End Sub
+
+        Protected Overrides Sub OnGroupChanged(ByVal pageGroup As RibbonPageGroup)
+            MyBase.OnGroupChanged(pageGroup)
+            If pageGroup.AllowMinimize Then pageGroup.AllowMinimize = False
+        End Sub
+    End Class
 End Namespace
